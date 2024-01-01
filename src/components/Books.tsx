6648688e8563e8bf25bf31f363e27eab86f38435
elosis <96085941+elosis@users.noreply.github.com>
2024-01-02 01:40:06 +0300

    update 02.01.2024

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "~bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Books = () => {
   return (
@@ -15,8 +15,19 @@ const Books = () => {
         id="carouselExampleDark"
         className="carousel carousel-dark slide mt-5"
       >
-        <div className="carousel-inner">
-          <i class="bi bi-bookshelf"></i>
+        <div className="carousel-inner d-flex flex-row justify-content-center gap-4">
+          <div className="d-flex flex-column">
+            <i className="bi bi-bookshelf custom-icon" />
+            <span>BOOKSHELF</span>
+          </div>
+          <div className="d-flex flex-column">
+            <i className="bi bi-person-square custom-icon" custom-icon></i>
+            <span>MEMBERSHIP</span>
+          </div>
+          <div className="d-flex flex-column">
+            <i className="bi bi-wallet-fill custom-icon"></i>
+            <span>BESTSELLERS https://www.barnesandnoble.com/</span>
+          </div>
         </div>
         <button
           className="carousel-control-prev"
