07a50e29d4be114c62eb70a42a918f44370acaea
elosis <96085941+elosis@users.noreply.github.com>
2024-01-31 00:11:18 +0300

    Fiction.tsx

---
+++ src/pages/Fiction.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const Fiction = () => {
+  return <div></div>;
+};
