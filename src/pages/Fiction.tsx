f48b1f41b27f07eac3ae0426eef5779da29de7c8
elosis <96085941+elosis@users.noreply.github.com>
2024-02-04 00:47:25 +0300

    Fiction css final

--- src/pages/Fiction.tsx
+++ src/pages/Fiction.tsx
@@ -3,10 +3,85 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Fiction = () => {
+  const fictionData = [
+    {
+      title: "Cleopatra and Frankenstein",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781639730704_p0_v5%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Coco Mellors",
+    },
+    {
+      title: "This Other Eden (B&N Exclusive Edition) by",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781324079538_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Paul Harding",
+    },
+    {
+      title: "Flight",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780063135154_p0_v6%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Lynn Steger Strong",
+    },
+    {
+      title: "Ladies' Lunch: And Other Stories",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781324079538_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Lore Segal",
+    },
+    {
+      title: "The Old Place",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780593422366_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Bobby Finger",
+    },
+    {
+      title: "  Now Is Not the Time to Panic",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780062913517_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Kevin Wilson",
+    },
+    {
+      title: "  The Rabbit Hutch (BS Discover Prize Winner)",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780593467879_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Tess Gunty",
+    },
+    {
+      title: "  The Second Ending (BS Exclusive Edition)",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780593724941_p0_v2%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Michelle Hoffman",
+    },
+    {
+      title: "  Notes on Your Sudden Disappearanc: A Novel by",
+      imageUrl:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781250871442_p0_v12%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      author: " Alison Espach",
+    },
+  ];
+
+  const cardsPerPage = 3;
+  const [visibleCardIndexes, setVisibleCardIndexes] = useState([0, 1, 2]);
+
+  const handleNextButtonClick = () => {
+    const nextIndexes = visibleCardIndexes.map(
+      (index) => (index + 1) % fictionData.length
+    );
+    setVisibleCardIndexes(nextIndexes);
+  };
+
+  const handlePrevButtonClick = () => {
+    const prevIndexes = visibleCardIndexes.map(
+      (index) => (index - 1 + fictionData.length) % fictionData.length
+    );
+    setVisibleCardIndexes(prevIndexes);
+  };
+
   return (
-    <div>
+    <div className="mx-auto" style={{ maxWidth: "1436px" }}>
       <img
         src="https://dispatch.barnesandnoble.com/content/dam/ccr/global/global-nav-banner/2024/01/PROD-28717_RUSH_Global_Nav_Post-Holiday-Sale_01-27_r6.jpg"
+        className="w-100"
         alt=""
       ></img>
 
@@ -173,7 +248,7 @@ const Fiction = () => {
           <span>Our Fiction Monthly Picks</span>
         </div>
         <div className="d-flex">
-          <div>
+          <div style={{ borderRight: "1px solid black", paddingRight: "15px" }}>
             <h5>This Month's Pick: Cleopatra and Frankenstein:</h5>
             <p className="mt-4">
               Darkly entertaining, subtly heartbreaking, but nonetheless a joy
@@ -182,119 +257,37 @@ const Fiction = () => {
               humorous and deeply human narrative voice.
             </p>
           </div>
-          <div className="d-flex">
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781639730704_p0_v5%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  Cleopatra and Frankenstein by <strong>Coco Mellors</strong>
-                </p>
-              </div>
+          <div className="d-flex" style={{ paddingLeft: "15px" }}>
+            <div
+              className="left-arrow-container"
+              onClick={handlePrevButtonClick}
+            >
+              <div className="left-arrow-head"></div>
             </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781324079538_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  This Other Eden (B&N Exclusive Edition) by{" "}
-                  <strong>Paul Harding</strong>
-                </p>
+            {visibleCardIndexes.map((index) => (
+              <div
+                key={index}
+                className={`card mx-2`}
+                style={{ width: "18rem", border: "none" }}
+              >
+                <img
+                  src={fictionData[index].imageUrl}
+                  className="card-img-top w-50 mx-auto cards-img"
+                  alt="Book Cover"
+                />
+                <div className="card-body text-center">
+                  <div className="card-title">{fictionData[index].title}</div>
+                  <p className="card-text">
+                    by <strong>{fictionData[index].author}</strong>
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780063135154_p0_v6%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  Flight by <strong>Lynn Steger Strong</strong>
-                </p>
-              </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781685891015_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  Ladies' Lunch: And Other Stories by{" "}
-                  <strong>Lore Segal</strong>
-                </p>
-              </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780593422366_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  The Old Place <strong>by Bobby Finger</strong>
-                </p>
-              </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780062913517_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  Now Is Not the Time to Panic by <strong>Kevin Wilson</strong>
-                </p>
-              </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780593467879_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  The Rabbit Hutch (BS Discover Prize Winner) by
-                  <strong> Tess Gunty</strong>
-                </p>
-              </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9780593724941_p0_v2%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  The Second Ending (BS Exclusive Edition) by
-                  <strong> Michelle Hoffman</strong>
-                </p>
-              </div>
-            </div>
-            <div className="card" style={{ width: "18rem", border: "none" }}>
-              <img
-                src="https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&product=path%5B/pimages/9781250871442_p0_v12%5D&call=url%5Bfile:common/decodeProduct.chain%5D"
-                className="card-img-top w-50 mx-auto cards-img"
-                alt="..."
-              />
-              <div className="card-body text-center">
-                <p className="card-title footer-list">
-                  Notes on Your Sudden Disappearanc: A Novel by
-                  <strong> Alison Espach</strong>
-                </p>
-              </div>
+            ))}
+            <div
+              className="right-arrow-container"
+              onClick={handleNextButtonClick}
+            >
+              <div className="right-arrow-head"></div>
             </div>
           </div>
         </div>
